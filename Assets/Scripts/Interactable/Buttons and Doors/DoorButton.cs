bc2f19e8edafb93a71d7ffc39dd0a30f2d644b8f
Elliott Kelly <ek37@iu.edu>
2025-03-11 17:41:05 -0400

    Added button sprites and created prefabs for them
    The Player Button prefab is a button that can be activated by the player only.
    The Enemy Button prefab is a button that can be activated by the enemy only.

--- Assets/Scripts/Interactable/Buttons and Doors/DoorButton.cs
+++ Assets/Scripts/Interactable/Buttons and Doors/DoorButton.cs
@@ -13,11 +13,19 @@ public class DoorButton : MonoBehaviour
         EnemyButton
     }
     [SerializeField] ButtonTypes type;
+    [SerializeField] Sprite pressedSprite;
     [SerializeField] GameObject[] doors;
     // Start is called before the first frame update
     void Start()
     {
-        doors = GameObject.FindGameObjectsWithTag("Player Door");
+        if (type == ButtonTypes.PlayerButton)
+        {
+            doors = GameObject.FindGameObjectsWithTag("Player Door");
+        }
+        else if (type == ButtonTypes.EnemyButton)
+        {
+            doors = GameObject.FindGameObjectsWithTag("Enemy Door");
+        }
     }
 
     // Update is called once per frame
@@ -36,6 +44,18 @@ public class DoorButton : MonoBehaviour
                 {
                     door.GetComponent<Door>().Open();
                 }
+                GetComponent<SpriteRenderer>().sprite = pressedSprite;
+            }
+        }
+        else if(type == ButtonTypes.EnemyButton)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.GetComponent<Door>().Open();
+                }
+                GetComponent<SpriteRenderer>().sprite = pressedSprite;
             }
         }
     }
